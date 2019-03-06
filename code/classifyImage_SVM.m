c1c204fb632a6d47e27bb18de92f330c1566f0c3
Thomas Pickup <hello@thomaspickup.co.uk>
2019-03-06 10:42:44 +0000

    Added script for training and testing an SVM classifier, allowing for evaluation of the accuracy of an SVM model.

---
+++ code/classifyImage_SVM.m
@@ -0,0 +1,35 @@
+% Trains and tests an SVM model for classifying images as face or non-face.
+% Uses full image as feature descriptor.
+clearvars
+close all
+
+%% training
+
+% Load the training images & labels
+[images,labels] = loadFaceImages('face_train.cdataset',1);
+
+% Train the SVM model
+modelSVM = SVMTraining(images, labels);
+
+%% testing
+
+[images,labels] = loadFaceImages('face_test.cdataset',1);
+
+% Classify each testing image using the trained model
+
+% Preallocate results array
+results = zeros(size(images,1),1);
+
+for i=1:size(images,1)
+    test_image = images(i,:);
+    results(i) = SVMTesting(test_image,modelSVM);
+end
+
+%% evaluation
+
+% Compare given classification of each testing image to its true class
+comparison = (labels==results);
+
+% Calculate accuracy of the model
+accuracy = sum(comparison) / length(comparison);
+fprintf('Accuracy of SVM model: %.2f%%\n', 100*accuracy);
