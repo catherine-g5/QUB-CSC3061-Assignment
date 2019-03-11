abc8996c5567ef25decd1ff1efb57d7394e173fe
Thomas Pickup <40145342@eeecs.qub.ac.uk>
2019-03-11 18:13:36 +0000

    Refactoring Changed classifyImage_SVM to fullImage_SVM and copied reducedDimensionality_KNN to fullImage_KNN Changes to be committed: 	new file:   code/fullImage_KNN.m 	renamed:    code/classifyImage_SVM.m -> code/fullImage_SVM.m

---
+++ code/fullImage_KNN.m
@@ -0,0 +1,25 @@
+clearvars
+close all
+
+addpath('images');
+kValue = [1, 3, 5, 10];
+
+[trainImages,trainLabels] = loadFaceImages('face_train.cdataset',1);
+modelNN = KNNTraining(trainImages, trainLabels);
+
+[testImages,testLabels] = loadFaceImages('face_test.cdataset',1);
+for k = 1:numel(kValue)
+    results = zeros(size(testImages,1),1);
+
+    for i=1:size(testImages,1)
+        test_image = testImages(i,:);
+        results(i) = KNNTesting(test_image,modelNN, kValue(k));
+    end
+
+    comparison = (testLabels==results);
+
+    accuracy = sum(comparison) / length(results);
+    fprintf('KNN with N equal to %d \n', kValue(k));
+    fprintf('Accuracy of KNN model: %.2f%%\n', 100*accuracy);
+end
+save reducedDimensionality_NN modelNN 
